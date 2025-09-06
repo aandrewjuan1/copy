2517bf65d46ce81b910df42242bfb8f6ca15c1c8
Andrew <117120195+aandrewjuan1@users.noreply.github.com>
2025-09-06 10:06:38 +0800

    Update and rename auth.md to auth.php

---
+++ auth.php
@@ -0,0 +1,13 @@
+    public function loginUser($email, $password) {
+        $sql = "SELECT user_id, username, password, is_admin FROM school_publication_users WHERE email = ?";
+        $user = $this->executeQuerySingle($sql, [$email]);
+
+        if ($user && password_verify($password, $user['password'])) {
+            $this->startSession();
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['is_admin'] = (bool)$user['is_admin'];
+            return true;
+        }
+        return false;
+    }
